ac72db9054f0d3c37f47a6b63289450e37a9cf3b
RobertoJavaDev <robertojavadev@gmail.com>
2022-07-07 07:11:21 +0200

    initial commit

---
+++ src/main/java/pl/robertojavadev/motorshopapp/MotorShopAppApplication.java
@@ -0,0 +1,13 @@
+package pl.robertojavadev.motorshopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorShopAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotorShopAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/robertojavadev/motorshopapp/MotorShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.robertojavadev.motorshopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorShopAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
